fa9bacfbd7161a57fe5d75135ae06f0c18c4e91c
bfk <bfker@qq.com>
2020-06-07 23:46:23 +0800

    提交

---
+++ lab10/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'art');
+define('DBUSER', 'testuser');
+define('DBPASS', 'Betty123456815');
+define('DBCONNSTRING','mysql:dbname=art;charset=utf8mb4;');
+?>
